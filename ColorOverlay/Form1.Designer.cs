f8b3076da1b69002187f1c5f18b0c979d20eade3
Shilyx <shilyx@outlook.com>
2019-07-29 11:21:13 +0800

    half rgb

--- ColorOverlay/Form1.Designer.cs
+++ ColorOverlay/Form1.Designer.cs
@@ -28,20 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnOperator = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(207, 326);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel_MouseClick);
+            // 
+            // btnOperator
+            // 
+            this.btnOperator.Enabled = false;
+            this.btnOperator.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnOperator.Location = new System.Drawing.Point(239, 156);
+            this.btnOperator.Name = "btnOperator";
+            this.btnOperator.Size = new System.Drawing.Size(38, 32);
+            this.btnOperator.TabIndex = 1;
+            this.btnOperator.Text = "+";
+            this.btnOperator.UseVisualStyleBackColor = true;
+            this.btnOperator.Click += new System.EventHandler(this.btnOperator_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(295, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(207, 326);
+            this.panel2.TabIndex = 1;
+            this.panel2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel_MouseClick);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Enabled = false;
+            this.btnCalc.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCalc.Location = new System.Drawing.Point(525, 156);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(38, 32);
+            this.btnCalc.TabIndex = 1;
+            this.btnCalc.Text = "=";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Enabled = false;
+            this.panel3.Location = new System.Drawing.Point(584, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(207, 326);
+            this.panel3.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(717, 303);
+            this.ClientSize = new System.Drawing.Size(808, 350);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.btnOperator);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "颜色叠加 - 取平均值版本";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnOperator;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Panel panel3;
     }
 }
 
--- ColorOverlay/Form1.cs
+++ ColorOverlay/Form1.cs
@@ -11,9 +11,60 @@ namespace ColorOverlay
 {
     public partial class Form1 : Form
     {
+        private bool bAdd = true;
+
         public Form1()
         {
             InitializeComponent();
+            panel1.BackColor = Color.Blue;
+            panel2.BackColor = Color.Red;
+            calc();
+        }
+
+        private void btnOperator_Click(object sender, EventArgs e)
+        {
+            bAdd = !bAdd;
+            btnOperator.Text = bAdd ? "+" : "-";
+            calc();
+        }
+
+        private void calc()
+        {
+            int r = Math.Min((panel1.BackColor.R + panel2.BackColor.R) / 2, 255);
+            int g = Math.Min((panel1.BackColor.G + panel2.BackColor.G) / 2, 255);
+            int b = Math.Min((panel1.BackColor.B + panel2.BackColor.B) / 2, 255);
+
+            panel3.BackColor = Color.FromArgb(Convert.ToByte(r), Convert.ToByte(g), Convert.ToByte(b));
+        }
+
+        private void panel_MouseClick(object sender, MouseEventArgs e)
+        {
+            Panel p = sender as Panel;
+            Color c = ChooseColor(p.BackColor);
+
+            if (c == Color.Empty)
+            {
+                return;
+            }
+
+            p.BackColor = c;
+            p.Update();
+
+            calc();
+        }
+
+        private static Color ChooseColor(Color colorInit)
+        {
+            ColorDialog colorForm = new ColorDialog();
+
+            colorForm.Color = colorInit;
+
+            if (colorForm.ShowDialog() == DialogResult.OK)
+            {
+                return colorForm.Color;
+            }
+
+            return Color.Empty;
         }
     }
 }
